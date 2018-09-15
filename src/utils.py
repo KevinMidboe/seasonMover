d50b97daa3e4744277f963b992b5e08a30b37b2a
Kevin Midbøe <kevin.midboe@gmail.com>
2018-09-15 12:44:15 +0200

    Changed all logger instances with the default logging.

--- src/utils.py
+++ src/utils.py
@@ -9,8 +9,6 @@ import struct
 from babelfish import Error as BabelfishError, Language
 from enzyme import MKV
 
-logger = logging.getLogger(__name__)
-
 def sanitize(string, ignore_characters=None):
     """Sanitize a string to strip special characters.
 
@@ -72,20 +70,20 @@ def refine(video, embedded_subtitles=True, **kwargs):
                     video.resolution = '%di' % video_track.height
                 else:
                     video.resolution = '%dp' % video_track.height
-                logger.debug('Found resolution %s', video.resolution)
+                logging.debug('Found resolution %s', video.resolution)
 
             # video codec
             if video_track.codec_id == 'V_MPEG4/ISO/AVC':
                 video.video_codec = 'h264'
-                logger.debug('Found video_codec %s', video.video_codec)
+                logging.debug('Found video_codec %s', video.video_codec)
             elif video_track.codec_id == 'V_MPEG4/ISO/SP':
                 video.video_codec = 'DivX'
-                logger.debug('Found video_codec %s', video.video_codec)
+                logging.debug('Found video_codec %s', video.video_codec)
             elif video_track.codec_id == 'V_MPEG4/ISO/ASP':
                 video.video_codec = 'XviD'
-                logger.debug('Found video_codec %s', video.video_codec)
+                logging.debug('Found video_codec %s', video.video_codec)
         else:
-            logger.warning('MKV has no video track')
+            logging.warning('MKV has no video track')
 
         # main audio track
         if mkv.audio_tracks:
@@ -93,15 +91,15 @@ def refine(video, embedded_subtitles=True, **kwargs):
             # audio codec
             if audio_track.codec_id == 'A_AC3':
                 video.audio_codec = 'AC3'
-                logger.debug('Found audio_codec %s', video.audio_codec)
+                logging.debug('Found audio_codec %s', video.audio_codec)
             elif audio_track.codec_id == 'A_DTS':
                 video.audio_codec = 'DTS'
-                logger.debug('Found audio_codec %s', video.audio_codec)
+                logging.debug('Found audio_codec %s', video.audio_codec)
             elif audio_track.codec_id == 'A_AAC':
                 video.audio_codec = 'AAC'
-                logger.debug('Found audio_codec %s', video.audio_codec)
+                logging.debug('Found audio_codec %s', video.audio_codec)
         else:
-            logger.warning('MKV has no audio track')
+            logging.warning('MKV has no audio track')
 
         # subtitle tracks
         if mkv.subtitle_tracks:
@@ -112,19 +110,19 @@ def refine(video, embedded_subtitles=True, **kwargs):
                         try:
                             embedded_subtitle_languages.add(Language.fromalpha3b(st.language))
                         except BabelfishError:
-                            logger.error('Embedded subtitle track language %r is not a valid language', st.language)
+                            logging.error('Embedded subtitle track language %r is not a valid language', st.language)
                             embedded_subtitle_languages.add(Language('und'))
                     elif st.name:
                         try:
                             embedded_subtitle_languages.add(Language.fromname(st.name))
                         except BabelfishError:
-                            logger.debug('Embedded subtitle track name %r is not a valid language', st.name)
+                            logging.debug('Embedded subtitle track name %r is not a valid language', st.name)
                             embedded_subtitle_languages.add(Language('und'))
                     else:
                         embedded_subtitle_languages.add(Language('und'))
-                logger.debug('Found embedded subtitle %r', embedded_subtitle_languages)
+                logging.debug('Found embedded subtitle %r', embedded_subtitle_languages)
                 video.subtitle_languages |= embedded_subtitle_languages
         else:
-            logger.debug('MKV has no subtitle track')
+            logging.debug('MKV has no subtitle track')
     else:
-        logger.debug('Unsupported video extension %s', extension)
+        logging.debug('Unsupported video extension %s', extension)
