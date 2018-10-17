37a0c6f62bc4b5d04d602c370473c69ce13794e6
KevinMidboe <kevin.midboe@gmail.com>
2018-10-17 23:29:09 +0200

    Will iterate over all files until all have a files have sufficient info.

--- src/cli.py
+++ src/cli.py
@@ -4,14 +4,22 @@ from core import scan_folder, moveHome
 from video import Video
 from guessit import guessit
 
+from exceptions import InsufficientInfoError
+
 videos, insufficient_info = scan_folder('Spider.Man')
 print('Sweet lemonade: {} {}'.format(videos, insufficient_info))
 
 for video in videos:
     moveHome(video)
 
-for file in insufficient_info:
-    supplementary_info = input("Insufficient info for match file: '{}'\nSupplementary info: ".format(file)) 
-    print(supplementary_info)
-    video = Video.fromguess(file, guessit(supplementary_info))
-    moveHome(video)
+while len(insufficient_info) > 1:
+    for file in insufficient_info:
+        supplementary_info = input("Insufficient info for match file: '{}'\nSupplementary info: ".format(file)) 
+        print(supplementary_info)
+        try:
+            video = Video.fromguess(file, guessit(supplementary_info))
+            insufficient_info.pop()
+        except InsufficientInfoError:
+            pass
+            
+        moveHome(video)
